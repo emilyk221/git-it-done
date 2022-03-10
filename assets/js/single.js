302818ce4bc6c5e0198791c827cbf548fc6e3946
emilyk <emily.g.kruse@gmail.com>
2022-03-09 21:12:19 -0500

    add warning message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 let issueContainerEl = document.querySelector("#issues-container");
+let limitWarningEl = document.querySelector("#limit-warning");
 
 let getReposIssues = function(repo) {
   let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +10,11 @@ let getReposIssues = function(repo) {
       response.json().then(function(data) {
         // pass response data to dom function
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       })
     }
     else {
@@ -55,4 +61,17 @@ let displayIssues = function(issues) {
   }
 }
 
-getReposIssues("emilyk221/code-quiz");
+let displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  let linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+}
+
+getReposIssues("facebook/react");
